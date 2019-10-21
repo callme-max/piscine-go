3b0419b2ef1d2a401e6f06aeb116758ced453c4c
callme-max <aiman.makhsudova@nu.edu.kz>
2019-10-21 14:48:23 +0600

    iter fact

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,8 +3,16 @@ package piscine
 func IterativeFactorial(nb int) int {
 	result := 1
 
+	if nb < 0 {
+		result = 0
+		return result
+	}
 	for i := 1; i < nb; i++ {
 		result = result * (i + 1)
 	}
+	if result > 0 && nb < 2000000 {
+		return result
+	}
+	result = 0
 	return result
 }
